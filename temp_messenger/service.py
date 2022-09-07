459168632654dad1a39f413f64e330b4f7be62bc
Sonola Moyo <sonolamoyosoluwalorun@gmail.com>
2022-09-07 17:03:35 +0100

    connecting nameko to redis(an in-memory-storage)

--- temp_messenger/service.py
+++ temp_messenger/service.py
@@ -1,9 +1,22 @@
 from nameko.rpc import rpc, RpcProxy
 from nameko.web.handlers import http
+from temp_messenger.dependencies.redis import Redis
 
-class KonnichiwaService:
-    name = 'konnichiwa_service'
+# class KonnichiwaService:
+#     name = 'konnichiwa_service'
 
+#     @rpc
+#     def konnichiwa(self):
+#         return 'Konnichiwa!'
+
+class MessageService:
+    name = 'message_service'
+    redis = Redis()
+    
+    @rpc
+    def get_message(self, message_id):
+        return self.redis.get_message(message_id) 
+    
     @rpc
     def konnichiwa(self):
         return 'Konnichiwa!'
