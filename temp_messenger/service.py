1c3a7819ccddf8b997799f2a449fe7fb91fc06a8
Sonola Moyo <sonolamoyosoluwalorun@gmail.com>
2022-09-07 19:42:18 +0100

    template setup

--- temp_messenger/service.py
+++ temp_messenger/service.py
@@ -1,6 +1,9 @@
+import json
 from nameko.rpc import rpc, RpcProxy
 from nameko.web.handlers import http
 from temp_messenger.dependencies.redis import Redis
+from temp_messenger.dependencies.jinja2 import Jinja2
+from werkzeug.wrappers import Response
 
 # class KonnichiwaService:
 #     name = 'konnichiwa_service'
@@ -33,9 +36,44 @@ class MessageService:
 
 class WebServer:
     name = 'web_server'
-    konnichiwa_service = RpcProxy('konnichiwa_service')
+    # konnichiwa_service = RpcProxy('konnichiwa_service')
+    
+    # @http('GET', '/')
+    # def root(self, request):
+    #     return self.konnichiwa_service.konnichiwa()
+    
+    message_service = RpcProxy('message_service')
+    templates = Jinja2()
     
     @http('GET', '/')
-    def root(self, request):
-        return self.konnichiwa_service.konnichiwa()
+    def home(self, request):
+        messages = self.message_service.get_all_messages()
+        rendered_template = self.templates.render_home(messages)
+        html_response = create_html_response(rendered_template)
+        return html_response
+    
+    @http('POST', '/')
+    def post_message(self, request):
+        try:
+            data = get_request_data(request)
+        except json.JSONDecodeError:
+            return 400, 'JSON payload expected'
+        
+        try: 
+            message = data['message']
+        except KeyError:
+            return 400, 'No message given'
+        
+        self.message_service.save_message(message)
+        
+        return 204, ''
 
+
+def create_html_response(content):
+    headers = {'Content-Type': 'text/html'}
+    return Response(content, status=200, headers=headers)
+
+def get_request_data(request):
+    data_as_text = request.get_data(as_text=True)
+    return json.loads(data_as_text)
+    
